6951bd26532aead7f2e75a93517f2ab16f980efd
Vroomfrondal <lionscantcode@gmail.com>
2023-08-06 13:24:23 -0500

    feat: rate-limiting,

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,21 +1,31 @@
 import { Router } from 'express'
 import axios from 'axios'
+import url from 'url'
+// import apicache from 'apicache'
 require('dotenv').config()
 
 export const imageRoute = Router()
 
+// Init cache.
+// let cache = apicache.middleware
+
+// cache('2 minutes')
 imageRoute.get('/images', async (req, res) => {
   const URL = process.env.NASA_BASE_URL!
   const KEY = process.env.NASA_API_KEY!
 
   try {
+    const dynamicURLParams = url.parse(req.url, true).query
+    console.log(dynamicURLParams)
+
     const CONFIG = {
-      params: { api_key: KEY, start_date: '2022-10-26', end_date: '2022-11-2' },
+      params: { api_key: KEY, start_date: dynamicURLParams.start_date, end_date: dynamicURLParams.end_date },
     }
     const request = await axios.get(URL, CONFIG)
 
     if (request.status === 200) {
       const response = await request.data.reverse() // reversing so today's image shows first
+
       res.status(200).json(response)
     } else {
       res.status(400).json({ err: `Failed with status code: ${request.status}, Text: ${request.statusText}` })
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import express from 'express'
 import cors from 'cors'
+import { rateLimit } from 'express-rate-limit'
 import { routes } from './routes'
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
 const app = express()
 
-// Routes
+// Rate Limiting Middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // window for requests
+  max: 100, // max requests
+  standardHeaders: true,
+  legacyHeaders: false,
+})
+app.use(limiter)
+app.set('Trust Proxy', 1)
+
+// Set Static Folder
+app.use(express.static('public'))
+
+// Import Routes
 app.use('/', routes)
 
 // Enable Cors
