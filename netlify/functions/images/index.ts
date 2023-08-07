38b4c8be00b4663f51bb1f9623d879b60195745e
Vroomfrondal <lionscantcode@gmail.com>
2023-08-07 16:26:17 -0500

    Update index.ts

--- netlify/functions/images/index.ts
+++ netlify/functions/images/index.ts
@@ -1,37 +1,48 @@
 // serverless function for fetching NASA images
-
 import { Router } from 'express'
 import axios from 'axios'
 import url from 'url'
 require('dotenv').config()
 
-const imageRoute = Router()
-
-imageRoute.get('/images', async (req, res) => {
-  const URL = process.env.NASA_BASE_URL!
-  const KEY = process.env.NASA_API_KEY!
-
-  try {
-    const dynamicURLParams = url.parse(req.url, true).query
-    console.log(dynamicURLParams)
-
-    const CONFIG = {
-      params: { api_key: KEY, start_date: dynamicURLParams.start_date, end_date: dynamicURLParams.end_date },
-    }
-    const request = await axios.get(URL, CONFIG)
-
-    if (request.status === 200) {
-      const response = await request.data.reverse() // reversing so today's image shows first
-
-      res.status(200).json(response)
-    } else {
-      res.status(400).json({ err: `Failed with status code: ${request.status}, Text: ${request.statusText}` })
-    }
-  } catch (err: any) {
-    console.log(err)
-    res.status(500).json({ err })
-  }
-})
-
 // export default imageRoute
-export const handler = imageRoute
+// export const handler = imageRoute
+
+import type { Handler, HandlerEvent, HandlerContext } from '@netlify/functions'
+
+const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  // your server-side functionality
+  const imageRoute = Router()
+
+  imageRoute.get('/images', async (req, res) => {
+    const URL = process.env.NASA_BASE_URL!
+    const KEY = process.env.NASA_API_KEY!
+
+    try {
+      const dynamicURLParams = url.parse(req.url, true).query
+      console.log(dynamicURLParams)
+
+      const CONFIG = {
+        params: { api_key: KEY, start_date: dynamicURLParams.start_date, end_date: dynamicURLParams.end_date },
+      }
+      const request = await axios.get(URL, CONFIG)
+
+      if (request.status === 200) {
+        const response = await request.data.reverse() // reversing so today's image shows first
+
+        res.status(200).json(response)
+      } else {
+        res.status(400).json({ err: `Failed with status code: ${request.status}, Text: ${request.statusText}` })
+      }
+    } catch (err: any) {
+      console.log(err)
+      res.status(500).json({ err })
+    }
+  })
+
+  return {
+    statusCode: 200,
+    body: 'some value',
+  }
+}
+
+export { handler }
