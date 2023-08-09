1be1cd4dd787412917252004d10dc537b8f1cfca
Vroomfrondal <lionscantcode@gmail.com>
2023-08-09 11:49:07 -0500

    feat: Heroku

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,7 @@
 import { Router } from 'express'
 import { imageRoute } from './images'
-import { emailRoute } from './email'
 
 export const routes = Router()
 
 // Routes
 routes.use(imageRoute)
-routes.use(emailRoute)
